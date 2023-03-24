bde1b223f45957b0dac3a4eeb5ccaa3a1a067c13
Zoltán Raffay <villagerslovak@gmail.com>
2023-03-24 08:58:44 +0100

    remove unnecesary status print

--- src/main/java/sk/stuba/fei/uim/oop/game/Game.java
+++ src/main/java/sk/stuba/fei/uim/oop/game/Game.java
@@ -50,7 +50,6 @@ public class Game {
                     break;
                 }
                 currentPlayer.discardCards();
-                currentPlayer.status();
                 checkIfAPlayerDied();
             }
         }
